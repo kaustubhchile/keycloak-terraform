e44a574fb9e37babfffea243841946e20a793640
kaustubhchile <kaustubchile@gmail.com>
2026-04-05 01:17:57 +0530

    Update Kubernetes version

--- variables.tf
+++ variables.tf
@@ -45,6 +45,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
+  default     = "1.30"
 }
 
 variable "node_instance_type" {
